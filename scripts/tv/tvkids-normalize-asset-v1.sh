d2ff6d1533d125063e37c83790f2c534d9c0d19d
MarcoTuaTelecom <marcotuatelecom@gmail.com>
2026-09-10 16:12:06 -0300

    tv(tvkids): harden canonical timestamps with zero-offset no-B-frame output

--- scripts/tv/tvkids-normalize-asset-v1.sh
+++ scripts/tv/tvkids-normalize-asset-v1.sh
@@ -13,9 +13,9 @@ nice -n 19 ionice -c3 ffmpeg -hide_banner -nostdin -loglevel warning -y \
   -vf "setpts=PTS-STARTPTS,fps=30,scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:(ow-iw)/2:(oh-ih)/2,format=yuv420p" \
   -af "aresample=48000:async=1:first_pts=0,apad" \
   -c:v libx264 -preset veryfast -crf 18 -profile:v high -level 3.1 \
-  -g 60 -keyint_min 60 -sc_threshold 0 -threads 1 \
+  -g 60 -keyint_min 60 -sc_threshold 0 -bf 0 -threads 1 \
   -c:a aac -b:a 192k -ar 48000 -ac 2 \
-  -shortest -video_track_timescale 90000 -movflags +faststart \
+  -shortest -video_track_timescale 90000 -avoid_negative_ts make_zero -movflags +faststart \
   -map_metadata -1 -sn -dn "$tmp"
 ffmpeg -hide_banner -nostdin -v error -xerror -i "$tmp" -map 0:v:0 -map 0:a:0 -f null -
 mv -f -- "$tmp" "$dst"
