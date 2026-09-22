dcae2333722fe48a1573ec57279960721ac959be
MarcoTuaTelecom <marcotuatelecom@gmail.com>
2026-09-22 10:22:50 -0300

    ios: expose raw AAC and HLS URLs per station

--- StudioSAT-V2/native/ios/StudioSatV2/Station.swift
+++ StudioSAT-V2/native/ios/StudioSatV2/Station.swift
@@ -4,13 +4,17 @@ import Foundation
 struct Station: Identifiable, Hashable {
     let id: String
     let name: String
-    let streamURL: URL
+    let hlsURL: URL
+
+    var rawAACURL: URL {
+        URL(string: "https://www.radio.studiosatweb.com.br/listen-v2/live/\(id).aac")!
+    }
 
     static let all: [Station] = [
-        .init(id: "radioprincipal", name: "Rádio Studio Sat", streamURL: URL(string: "https://radio.studiosatweb.com.br/radioprincipal/index.m3u8")!),
-        .init(id: "radiopop", name: "Studio Sat Pop", streamURL: URL(string: "https://radio.studiosatweb.com.br/radiopop/index.m3u8")!),
-        .init(id: "radiorock", name: "Studio Sat Rock", streamURL: URL(string: "https://radio.studiosatweb.com.br/radiorock/index.m3u8")!),
-        .init(id: "radioclassicas", name: "Studio Sat Clássicas", streamURL: URL(string: "https://radio.studiosatweb.com.br/radioclassicas/index.m3u8")!),
-        .init(id: "radiocountry", name: "Studio Sat Country", streamURL: URL(string: "https://radio.studiosatweb.com.br/radiocountry/index.m3u8")!)
+        .init(id: "radioprincipal", name: "Rádio Studio Sat", hlsURL: URL(string: "https://radio.studiosatweb.com.br/radioprincipal/index.m3u8")!),
+        .init(id: "radiopop", name: "Studio Sat Pop", hlsURL: URL(string: "https://radio.studiosatweb.com.br/radiopop/index.m3u8")!),
+        .init(id: "radiorock", name: "Studio Sat Rock", hlsURL: URL(string: "https://radio.studiosatweb.com.br/radiorock/index.m3u8")!),
+        .init(id: "radioclassicas", name: "Studio Sat Clássicas", hlsURL: URL(string: "https://radio.studiosatweb.com.br/radioclassicas/index.m3u8")!),
+        .init(id: "radiocountry", name: "Studio Sat Country", hlsURL: URL(string: "https://radio.studiosatweb.com.br/radiocountry/index.m3u8")!)
     ]
 }
